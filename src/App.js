82fdfb4764b7c1116751ef0da6eb38ded28ca907
Hyonsok <46020558+Hyonsok@users.noreply.github.com>
2020-07-29 14:52:15 -0700

    make a movie list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-
+import Movie from './Movie';
+import "./App.css";
 //class component
 //to use state, make class component
 class App extends React.Component {
@@ -9,17 +10,38 @@ class App extends React.Component {
     movies : []
   };
   getMovies = async () => {
-    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json"); //to get api from the link, use axios
+    const {data: {data:{movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating"); //to get api from the link, use axios
+    this.setState({movies: movies, isLoading:false}) // first movies is from state, second is from axios but it can be just movies cuz they are the same
   }
+
   componentDidMount() {
     this.getMovies();
   }
+
   render() {
-    const {isLoading} = this.state;
+    const {isLoading, movies} = this.state;
     return (
-      <div>
-        {this.state.isLoading ? "Loading" : "We are ready"}
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ):(
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie 
+                key = {movie.id}
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary} 
+                poster={movie.medium_cover_image} 
+                genres = {movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
