0003a817eeec8c2d0775cb03c5fe81b705db5296
Hyonsok <46020558+Hyonsok@users.noreply.github.com>
2020-07-29 15:27:01 -0700

    use link,router

--- src/App.js
+++ src/App.js
@@ -1,49 +1,17 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import "./App.css";
-//class component
-//to use state, make class component
-class App extends React.Component {
-  state = {
-    isLoading :  true,
-    movies : []
-  };
-  getMovies = async () => {
-    const {data: {data:{movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating"); //to get api from the link, use axios
-    this.setState({movies: movies, isLoading:false}) // first movies is from state, second is from axios but it can be just movies cuz they are the same
-  }
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About"; //import {About as Contact} from  "..."; as can be used
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
 
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const {isLoading, movies} = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader_text">Loading...</span>
-          </div>
-        ):(
-          <div className="movies">
-            {movies.map(movie => (
-              <Movie 
-                key = {movie.id}
-                id={movie.id} 
-                year={movie.year} 
-                title={movie.title} 
-                summary={movie.summary} 
-                poster={movie.medium_cover_image} 
-                genres = {movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+  <HashRouter>
+    <Navigation/>
+    <Route path="/" exact={true} component={Home}/>
+    <Route  path="/about" component={About}/>
+  </HashRouter>
+  );
 }
 
-export default App;
+export default App; 
